7073adb45fd7417cb32df1669097a0a317a7f5d0
eriklarsson00 <71325754+eriklarsson00@users.noreply.github.com>
2022-03-29 11:36:30 +0200

    Scanner and camera

--- client/App.js
+++ client/App.js
@@ -1,12 +1,13 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import ImagePicker from './Components/ImagePicker';
+import LoginScreen from './Screens/LoginScreen';
+import BarCodeScanner from './Components/BarCodeScanner';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <BarCodeScanner />
   );
 }
 
