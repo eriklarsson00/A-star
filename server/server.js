380de9ee82d83c5a2d1aa44ee4358c03c18336ec
FKnorring <fille.vk@gmail.com>
2022-03-29 13:33:45 +0200

    Get product from database

--- server/server.js
+++ server/server.js
@@ -1,15 +1,22 @@
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+
+import 'dotenv/config'
+
 const express = require("express");
 const http = require("http");
-const dbconfig = require("./dbconfig").config;
 const WebSocket = require("ws");
-
 const app = express();
 const server = http.createServer(app);
 const wss = new WebSocket.Server({ server });
-const knex = require("knex")({
-  client: "mysql2",
-  connection: dbconfig,
-});
+
+import { getUser, getUsers, getUserEmail, addUser, updateUser, deleteUser } from "./routes/users.js"
+import { userExists } from "./routes/login.js";
+import { getCommunities, getCommunity } from "./routes/communities.js";
+import { getOffers, getOffer } from "./routes/offers.js"
+import { getRequests, getRequest } from "./routes/requests.js"
+import { getTransactions, getTransaction, getResponderTransactions, getListerTransactions } from "./routes/transactions.js"
+import { getProduct } from "./routes/products.js"
 
 app.use(express.json());
 
@@ -24,167 +31,57 @@ wss.on("connection", function connection(ws) {
   });
 });
 
-//GETS
+//PRODUCTS
+
+app.route("/products/:gtin").get(getProduct)
 
 //USERS
 
-app.get("/users", (req, res) => {
-  console.log("GET Users", new Date());
-  knex("Users")
-    .select()
-    .then((users) => {
-      res.json(users);
-    });
-});
+app.route("/users").get(getUsers).post(addUser);
 
-app.get("/users/:id", (req, res) => {
-  const id = req.params.id;
-  console.log("GET Users/" + id, new Date());
-  knex("Users")
-    .select()
-    .where("id", id)
-    .then((users) => {
-      res.json(users);
-    });
-});
+app
+  .route("/users/:id")
+  .get(getUser)
+  .put(updateUser)
+  .delete(deleteUser);
 
-app.get("/users/name/:name", (req, res) => {
-  const name = req.params.name;
-  console.log("GET Users/name/" + name, new Date());
-  knex("Users")
-    .select()
-    .where("firstname", name)
-    .then((users) => {
-      res.json(users);
-    });
-});
+app.route("/users/email/:email").get(getUserEmail);
+
+//LOGIN
+
+app.route("/login").post(userExists);
 
 //COMMUNITIES
 
-app.get("/communities", (req, res) => {
-  console.log("GET Communities", new Date());
-  knex("Communities")
-    .select()
-    .then((communities) => {
-      res.json(communities);
-    });
-});
+app.route("/communities").get(getCommunities);
 
-app.get("/communities/:id", (req, res) => {
-  const id = req.params.id;
-  console.log("GET Communities/" + id, new Date());
-  knex("Communities")
-    .select()
-    .where("id", id)
-    .then((communities) => {
-      res.json(communities);
-    });
-});
+app.route("/communities/:id").get(getCommunity);
 
 //OFFERS
 
-app.get("/offers", (req, res) => {
-  console.log("GET Offers", new Date());
-  knex("Offers")
-    .select()
-    .then((offers) => {
-      res.json(offers);
-    });
-});
+app.route("/offers").get(getOffers);
 
-app.get("/offers/:id", (req, res) => {
-  const id = req.params.id;
-  console.log("GET Offers/" + id, new Date());
-  knex("Offers")
-    .select()
-    .where("id", id)
-    .then((offers) => {
-      res.json(offers);
-    });
-});
+app.route("/offers/:id").get(getOffer);
 
 //REQUESTS
 
-app.get("/requests", (req, res) => {
-  console.log("GET Requests", new Date());
-  knex("Requests")
-    .select()
-    .then((requests) => {
-      res.json(requests);
-    });
-});
+app.route("/requests").get(getRequests);
 
-app.get("/requests/:id", (req, res) => {
-  const id = req.params.id;
-  console.log("GET Requests/" + id, new Date());
-  knex("Requests")
-    .select()
-    .where("id", id)
-    .then((requests) => {
-      res.json(requests);
-    });
-});
+app.route("/requests/:id").get(getRequest);
 
 //TRANSACTIONS
 
-app.get("/transactions", (req, res) => {
-  console.log("GET Transactions", new Date());
-  knex("Transactions")
-    .select()
-    .then((transactions) => {
-      res.json(transactions);
-    });
-});
+app.route("/transactions").get(getTransactions);
 
-app.get("/transactions/:id", (req, res) => {
-  const id = req.params.id;
-  console.log("GET Transactions/" + id, new Date());
-  knex("Transactions")
-    .select()
-    .where("id", id)
-    .then((transactions) => {
-      res.json(transactions);
-    });
-});
+app.route("/transactions/:id").get(getTransaction);
 
-app.get("/transactions/responder/:id", (req, res) => {
-  const id = req.params.id;
-  console.log("GET Transactions/responder/" + id, new Date());
-  knex("Transactions")
-    .select()
-    .where("responder_id", id)
-    .then((transactions) => {
-      res.json(transactions);
-    });
-});
+app
+  .route("/transactions/responder/:id")
+  .get(getResponderTransactions);
 
-app.get("/transactions/lister/:id", (req, res) => {
-  const id = req.params.id;
-  console.log("GET Transactions/responder/" + id, new Date());
-  knex("Transactions")
-    .select("Transactions.*")
-    .leftJoin("Offers", "Offers.id", "Transactions.offer_id")
-    .leftJoin("Requests", "Requests.id", "Transactions.request_id")
-    .where("Requests.user_id", id)
-    .orWhere("Offers.user_id", id)
-    .then((transactions) => {
-      res.json(transactions);
-    });
-});
+app.route("/transactions/lister/:id").get(getListerTransactions);
 
-//POSTS
-
-app.post("/users", (req, res) => {
-  const body = req.body;
-  knex("Users")
-    .insert(body)
-    .catch(() => {
-      res.sendStatus(404);
-    })
-    .then((id) => {
-      if (id !== undefined) res.json("User inserted with id: " + id);
-    });
-});
+//*************************SERVER*************************
 
 server.listen(8080, () => {
   console.log("Listening to port 8080");
