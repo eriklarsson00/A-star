9277e1ff830819522e1c5585f456e22fa8a80a19
Filip Palmqvist <fille.palmqvist@icloud.com>
2022-04-04 11:02:12 +0200

    fixed file encoding

--- client/Components/ImagePicker.component.js
+++ client/Components/ImagePicker.component.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import { View, Text, StyleSheet, Image, Button } from 'react-native';
-import * as ImagePicker from 'expo-image-picker';
-
+import React, { useState } from "react";
+import { View, Text, StyleSheet, Image, Button } from "react-native";
+import * as ImagePicker from "expo-image-picker";
 
 export default function ImagePickerComp() {
   // The path of the picked image
-  
-  const [pickedImagePath, setPickedImagePath] = useState('');
+
+  const [pickedImagePath, setPickedImagePath] = useState("");
 
   // This function is triggered when the "Select an image" button pressed
   const showImagePicker = async () => {
-    // Ask the user for the permission to access the media library 
-    const permissionResult = await ImagePicker.requestMediaLibraryPermissionsAsync();
+    // Ask the user for the permission to access the media library
+    const permissionResult =
+      await ImagePicker.requestMediaLibraryPermissionsAsync();
 
     if (permissionResult.granted === false) {
       alert("You've refused to allow this app to access your photos!");
@@ -24,11 +24,10 @@ export default function ImagePickerComp() {
     console.log(result);
 
     if (!result.cancelled) {
-     pushToServer(result);
-      };
-      console.log(result.uri);
+      pushToServer(result);
     }
-  
+    console.log(result.uri);
+  };
 
   // This function is triggered when the "Open camera" button pressed
   const openCamera = async () => {
@@ -41,7 +40,7 @@ export default function ImagePickerComp() {
     }
 
     const result = await ImagePicker.launchCameraAsync();
-  
+
     // Explore the result
     console.log(result);
     console.log("HIT?");
@@ -49,35 +48,26 @@ export default function ImagePickerComp() {
       console.log("HÄR DÅ?");
       pushToServer(result);
     }
-  }
+  };
 
   const pushToServer = (result) => {
     setPickedImagePath(result.uri);
-      const body = new FormData();
-      body.append('image', {
-        name: 'photo.jpg',
-        type: result.type,
-        uri: result.uri,
-      });
-      var ip = "http://ec2-3-215-18-23.compute-1.amazonaws.com/images";
-      console.log(ip);
-     
-      fetch(ip, {
-        method: 'POST',
-        body: body,
-        headers: {
-          "Content-Type": "multipart/form-data",
-        },
-      }).catch(err => console.log(err));
-    
-      
-     
-     
+    const body = new FormData();
+    body.append("image", {
+      name: "photo.jpg",
+      type: result.type,
+      uri: result.uri,
+    });
+    var ip = "http://ec2-3-215-18-23.compute-1.amazonaws.com/images";
 
-      
-    };
-  
-  
+    fetch(ip, {
+      method: "POST",
+      body: body,
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    }).catch((err) => console.log(err));
+  };
 
   return (
     <View style={styles.screen}>
@@ -87,37 +77,33 @@ export default function ImagePickerComp() {
       </View>
 
       <View style={styles.imageContainer}>
-        {
-          pickedImagePath !== '' && <Image
-            source={{ uri: pickedImagePath }}
-            style={styles.image}
-          />
-        }
+        {pickedImagePath !== "" && (
+          <Image source={{ uri: pickedImagePath }} style={styles.image} />
+        )}
       </View>
     </View>
   );
 }
-
 
 // Kindacode.com
 // Just some styles
 const styles = StyleSheet.create({
   screen: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
   },
   buttonContainer: {
     width: 400,
-    flexDirection: 'row',
-    justifyContent: 'space-around'
+    flexDirection: "row",
+    justifyContent: "space-around",
   },
   imageContainer: {
-    padding: 30
+    padding: 30,
   },
   image: {
     width: 400,
     height: 300,
-    resizeMode: 'cover'
-  }
+    resizeMode: "cover",
+  },
 });
