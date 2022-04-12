9853b759404e7d22407e1991f2e2f3021810d8dd
FKnorring <fille.vk@gmail.com>
2022-04-12 14:40:49 +0200

    Added logo and google icon

--- client/Screens/StartScreen.js
+++ client/Screens/StartScreen.js
@@ -1,6 +1,6 @@
 import React from "react";
-import { Text, Button } from "@ui-kitten/components";
-import { View } from "react-native";
+import { Text, Button, Icon } from "@ui-kitten/components";
+import { View, Image } from "react-native";
 import { UserInfo, UserLoggedIn, GoogleInfo } from "../assets/AppContext";
 import * as Google from "expo-auth-session/providers/google";
 import * as WebBrowser from "expo-web-browser";
@@ -10,92 +10,104 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 WebBrowser.maybeCompleteAuthSession();
 
 export const StartScreen = ({ navigation }) => {
-	const { userLoggedIn, setLoggedIn } = React.useContext(UserLoggedIn);
-	const { userInfo, setUserInfo } = React.useContext(UserInfo);
-	const { googleInfo, setGoogleInfo } = React.useContext(GoogleInfo);
-	const [accessToken, setAccessToken] = React.useState();
+  const { userLoggedIn, setLoggedIn } = React.useContext(UserLoggedIn);
+  const { userInfo, setUserInfo } = React.useContext(UserInfo);
+  const { googleInfo, setGoogleInfo } = React.useContext(GoogleInfo);
+  const [accessToken, setAccessToken] = React.useState();
 
-	const [request, response, promptAsync] = Google.useAuthRequest({
-		expoClientId:
-			"704852667516-v9g130n9d3shrms1np7t8aqfhqfcs4vv.apps.googleusercontent.com",
-	});
+  const [request, response, promptAsync] = Google.useAuthRequest({
+    expoClientId:
+      "704852667516-v9g130n9d3shrms1np7t8aqfhqfcs4vv.apps.googleusercontent.com",
+  });
 
-	React.useEffect(async () => {
-		if (googleInfo != null && googleInfo != undefined) {
-			let users = await getUserProfileByEmail(googleInfo.email);
+  React.useEffect(async () => {
+    if (googleInfo != null && googleInfo != undefined) {
+      let users = await getUserProfileByEmail(googleInfo.email);
 
-			if (users.length !== 0) {
-				try {
-					await AsyncStorage.setItem(
-						"userId",
-						JSON.stringify(users[0].id)
-					);
-					setUserInfo(users[0]);
+      if (users.length !== 0) {
+        try {
+          await AsyncStorage.setItem("userId", JSON.stringify(users[0].id));
+          setUserInfo(users[0]);
 
-					setLoggedIn(true);
-				} catch (e) {
-					// saving error
-					console.log(e);
-				}
-			} else {
-				navigation.navigate("CreateUserScreen");
-			}
-			return;
-		}
+          setLoggedIn(true);
+        } catch (e) {
+          // saving error
+          console.log(e);
+        }
+      } else {
+        navigation.navigate("CreateUserScreen");
+      }
+      return;
+    }
 
-		if (accessToken) {
-			getUserData();
-			return;
-		}
-		if (response?.type === "success") {
-			setAccessToken(response.authentication.accessToken);
-		}
-	}, [response, accessToken, googleInfo]);
+    if (accessToken) {
+      getUserData();
+      return;
+    }
+    if (response?.type === "success") {
+      setAccessToken(response.authentication.accessToken);
+    }
+  }, [response, accessToken, googleInfo]);
 
-	async function getUserData() {
-		let userInfoResponse = await fetch(
-			"https://www.googleapis.com/userinfo/v2/me",
-			{
-				headers: { Authorization: `Bearer ${accessToken}` },
-			}
-		);
-		userInfoResponse.json().then((data) => {
-			setGoogleInfo(data);
-		});
-	}
+  async function getUserData() {
+    let userInfoResponse = await fetch(
+      "https://www.googleapis.com/userinfo/v2/me",
+      {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      }
+    );
+    userInfoResponse.json().then((data) => {
+      setGoogleInfo(data);
+    });
+  }
 
-	const easySignIn = async () => {
-		let users = await getUserProfileByEmail("ansders.bagge@telia.com");
+  const easySignIn = async () => {
+    let users = await getUserProfileByEmail("ansders.bagge@telia.com");
 
-		if (users.length !== 0) {
-			try {
-				await AsyncStorage.setItem("userId", JSON.stringify(users[0].id));
-				setUserInfo(users[0]);
-				setLoggedIn(true);
-			} catch (e) {
-				// saving error
-				console.log(e);
-			}
-		}
-	};
+    if (users.length !== 0) {
+      try {
+        await AsyncStorage.setItem("userId", JSON.stringify(users[0].id));
+        setUserInfo(users[0]);
+        setLoggedIn(true);
+      } catch (e) {
+        // saving error
+        console.log(e);
+      }
+    }
+  };
 
-	return (
-		<View
-			style={{
-				alignContent: "center",
-				alignItems: "center",
-				marginTop: 550,
-			}}
-		>
-			<Button
-				onPress={() => {
-					promptAsync({ showInRecents: true });
-				}}
-				style={{ marginBottom: 14 }}
-			>
-				<Text>Continue with google</Text>
-			</Button>
-			<Button onPress={easySignIn}>Enkel inloggning</Button>
-		</View>
-	);
+  const GoogleIcon = (props) => <Icon name="google-outline" {...props} />;
+
+  return (
+    <View
+      style={{
+        alignContent: "center",
+        alignItems: "center",
+        marginTop: 150,
+      }}
+    >
+      <Image
+        source={require("../assets/icon.png")}
+        style={{
+          width: 200,
+          height: 200,
+          resizeMode: "stretch",
+          marginBottom: 200,
+        }}
+      ></Image>
+      <Button
+        accessoryLeft={GoogleIcon}
+        onPress={() => {
+          promptAsync({ showInRecents: true });
+        }}
+        style={{ marginBottom: 14 }}
+        status={"danger"}
+      >
+        <Text>Continue with Google</Text>
+      </Button>
+      <Button appearance={"ghost"} status={"info"} onPress={easySignIn}>
+        Enkel inloggning
+      </Button>
+    </View>
+  );
 };
