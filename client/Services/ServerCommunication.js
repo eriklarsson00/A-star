ab5be8c5b1913958a50a095b5b5de857854a7b53
Filip Palmqvist <33493180+mrPaintMan@users.noreply.github.com>
2022-04-26 11:23:35 +0200

    Update client/Services/ServerCommunication.js

--- client/Services/ServerCommunication.js
+++ client/Services/ServerCommunication.js
@@ -156,7 +156,6 @@ const pushImagesToServer = async (image, serverPath, userId) => {
 };
 
 const postOffer = async (offers, usercommunities) => {
-  console.log("inne i post offer ");
   const upload_obj = {
     offer: offers,
     communities: usercommunities,
