2051acfcd557bd4184ccae2e73ffec7aafed1252
eliind7 <elis.indebetou@hotmail.com>
2022-05-06 11:50:33 +0200

    stylat inväntar bekräftelse modal

--- client/Components/Modals/AwaitingConfirmationModal.js
+++ client/Components/Modals/AwaitingConfirmationModal.js
@@ -1,5 +1,6 @@
 import { Modal, Text, Layout, Button, Card } from "@ui-kitten/components";
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Image } from "react-native";
+import tw from "twrnc";
 import moment from "moment";
 
 export const AwaitingConfirmationModal = (props) => {
@@ -10,15 +11,42 @@ export const AwaitingConfirmationModal = (props) => {
       <View>
         <Text
           category={"h4"}
-          style={{ textAlign: "center", marginTop: 20, marginBottom: 25 }}
+          style={{ textAlign: "center", marginTop: 5, marginBottom: 15 }}
         >
           Inväntar bekräftelse
         </Text>
         {props.text}
-        <Text style={styles.space_between}>
+        <View
+          style={[
+            styles.image,
+            { alignItems: "center", justifyContent: "center" },
+          ]}
+        >
+          <Layout style={tw`py-8`}>
+            <Image
+              style={[tw`rounded-full`, styles.image, { marginTop: -18 }]}
+              source={{
+                uri: item.imgurl,
+                height: 100,
+                width: 100,
+              }}
+            />
+          </Layout>
+        </View>
+        <Text
+          style={[
+            styles.space_between,
+            {
+              textAlign: "center",
+              fontWeight: "bold",
+              fontSize: 20,
+              marginTop: -15,
+            },
+          ]}
+        >
           {item.firstname} {item.lastname}{" "}
         </Text>
-        <Layout style={{ flexDirection: "row", marginTop: 40 }}>
+        <Layout style={{ flexDirection: "row", marginTop: 20 }}>
           <Text category={"s1"}>Föreslaget utbyte: </Text>
           <Text>
             {moment(item.time_of_expiration).format("dddd Do MMM HH:mm")}
@@ -26,7 +54,7 @@ export const AwaitingConfirmationModal = (props) => {
         </Layout>
         <Button
           onPress={() => props.toggleModal(item)}
-          style={{ width: 120, marginTop: 120, alignSelf: "center" }}
+          style={{ width: 120, marginTop: 50, alignSelf: "center" }}
         >
           <Text>Stäng</Text>
         </Button>
